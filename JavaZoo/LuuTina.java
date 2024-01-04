6e8c342c3d41a37a62c9a2d11cb7d1e07e1b484f
Tuna w/ Black Airforces <143442984+Tuna336@users.noreply.github.com>
2024-01-04 11:37:25 -0800

    Add files via upload

---
+++ JavaZoo/LuuTina.java
@@ -0,0 +1,32 @@
+/**
+ * Write a description of class TinaLuu here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LuuTina extends Animal
+{
+   public LuuTina() {
+    super("Tina Luu", "Jet Blue", 12);
+  }
+  
+  public String getType() {
+    return "womper";
+  }
+  
+  public int getLegs() {
+    return 12;      
+  }
+  
+  public String speak() {
+    return "womp womp";
+  }
+ 
+  public String getDisposition() {
+    return "momp";
+  }
+  
+  public String getName() {
+    return "wommper";
+  }
+}
